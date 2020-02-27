17ec50fb1515a029576c406b2d309cf61ca2117c
warisa453 <61141431+warisa453@users.noreply.github.com>
2020-02-27 14:54:24 +0530

    Add files via upload

--- btre/settings.py
+++ btre/settings.py
@@ -141,12 +141,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 
 }
-#Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'traversy.brad@gmail.com'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+
 
 try:
     from .local_settings import *
